0f723e9945d1ae2ebf3ed80d068280a9a4c8d310
Vex <vox@voxclawd.ai>
2026-02-13 06:55:17 +0000

    Re-zoom tweet to 70% + bigger box (500px height, 350px visible)

--- frontend/src/components/TokenCard.jsx
+++ frontend/src/components/TokenCard.jsx
@@ -57,10 +57,10 @@ function TokenCard({ token }) {
           <iframe
             src={`https://platform.twitter.com/embed/Tweet.html?id=${token.tweetUrl.split('/').pop()}`}
             width="100%"
-            height="300"
+            height="500"
             frameBorder="0"
             scrolling="yes"
-            style={{ border: 'none', borderRadius: '8px', transform: 'scale(0.5)', transformOrigin: 'top left', width: '200%' }}
+            style={{ border: 'none', borderRadius: '8px', transform: 'scale(0.7)', transformOrigin: 'top left', width: '143%' }}
           ></iframe>
         </div>
       )}
